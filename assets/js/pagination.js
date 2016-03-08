165da12187cd39ad29d35e8affc9908a971e7583
Nikolay Matyunin <matyunin.n@gmail.com>
2016-03-08 17:23:52 +0100

    Redirect to 404.html for tag pages with wrong page number.

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -21,40 +21,40 @@ var do_paginate = function() {
     if (match !== null)
         page = parseInt(match[1]);
 
-// TODO: change hash to the first page
-    if (page > total_pages)
-        window.location.hash = '#page1';
+    if (page > 0 && page <= total_pages) {
+        var index_first = (page - 1) * paginate;
+        var index_last = page * paginate;
 
-    var index_first = (page - 1) * paginate;
-    var index_last = page * paginate;
-
-    var tt = 0;
-    var posts = document.querySelectorAll('.post');
-    for (var i = 0; i < posts.length; ++i) {
-        var index = posts[i].getAttribute('index') - 1;
-        if (index < index_first || index >= index_last) {
-            posts[i].style.display = 'none';
-            //document.getElementById('paginate_container').removeChild(posts[i]);
-        } else {
-            posts[i].style.display = '';
+        var tt = 0;
+        var posts = document.querySelectorAll('.post');
+        for (var i = 0; i < posts.length; ++i) {
+            var index = posts[i].getAttribute('index') - 1;
+            if (index < index_first || index >= index_last) {
+                posts[i].style.display = 'none';
+                //document.getElementById('paginate_container').removeChild(posts[i]);
+            } else {
+                posts[i].style.display = '';
+            }
         }
+
+        var pagHtml = '';
+        pagHtml = pagHtml + '<nav class="pagination" role="pagination">';
+        if (page > 1)
+            pagHtml = pagHtml + '<a class="newer-posts" href="' + url + '#page' + (page-1) + '/" title="Previous Page">&laquo; Newer Posts</a>';
+        pagHtml = pagHtml + '<span class="page-number"> Page ' + page + ' of ' + total_pages + ' </span>';
+        if (page < total_pages)
+            pagHtml = pagHtml + '<a class="older-posts" href="' + url + '#page' + (page+1) + '/" title="Next Page">Older Posts &raquo;</a>';
+        pagHtml = pagHtml + '</nav>';
+
+        if (page > 1)
+            document.getElementById('pagination-upper').innerHTML = pagHtml;
+        else
+            document.getElementById('pagination-upper').innerHTML = '';
+
+        document.getElementById('pagination-lower').innerHTML = pagHtml;
+    } else {
+        window.location = '/404.html';
     }
-
-    var pagHtml = '';
-    pagHtml = pagHtml + '<nav class="pagination" role="pagination">';
-    if (page > 1)
-        pagHtml = pagHtml + '<a class="newer-posts" href="' + url + '#page' + (page-1) + '/" title="Previous Page">&laquo; Newer Posts</a>';
-    pagHtml = pagHtml + '<span class="page-number"> Page ' + page + ' of ' + total_pages + ' </span>';
-    if (page < total_pages)
-        pagHtml = pagHtml + '<a class="older-posts" href="' + url + '#page' + (page+1) + '/" title="Next Page">Older Posts &raquo;</a>';
-    pagHtml = pagHtml + '</nav>';
-
-    if (page > 1)
-        document.getElementById('pagination-upper').innerHTML = pagHtml;
-    else
-        document.getElementById('pagination-upper').innerHTML = '';
-
-    document.getElementById('pagination-lower').innerHTML = pagHtml;
 };
 
 window.onhashchange = function() {
